1d761862d6efcc0e92067ba1557bef2eea4f51c9
Edswel <franckini@yahoo.com>
2024-02-27 10:48:01 +0100

    Add button and conditional rendering to friend list

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -5,6 +5,20 @@ const Friend = ({friend}) => {
     <li>
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className='red'>You owe {friend.name} ${Math.abs(friend.balance)}</p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className='green'>{friend.name} owes you ${Math.abs(friend.balance)}</p>
+      )}
+
+      {friend.balance === 0 && (
+        <p>You do not owe any money</p>
+      )}
+
+      <button className='button'>Select</button>
     </li>
   )
 }
