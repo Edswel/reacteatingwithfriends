395b1141f61376b6e96e70365306ed378a34892f
Edswel <franckini@yahoo.com>
2024-02-27 13:05:11 +0100

    Add functionality to add new friend

--- src/components/FormAddFriend.jsx
+++ src/components/FormAddFriend.jsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 
-const FormAddFriend = () => {
+const FormAddFriend = ({onAddFriend}) => {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const id = crypto.randomUUID()
+
+    const newFriend = {
+      name,
+      image: `${image}?=${id}`, 
+      balance: 0,
+      id
+    };
+
+    onAddFriend(newFriend);
+    
+    setName('');
+    setImage('https://i.pravatar.cc/48');
+  }
+
   return (
-    <form className='form-add-friend'>
+    <form className='form-add-friend' onSubmit={handleSubmit}>
       <label htmlFor="">🧑‍🤝‍🧑 Name</label>
-      <input type="text" />
+      <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
 
       <label htmlFor="">🖼️ Image</label>
-      <input type="text" />
+      <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
 
       <Button>Add</Button>
     </form>
